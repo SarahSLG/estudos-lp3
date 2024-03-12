41f61186ac9ef6fd61017de7445519e69adfe8d8
Sarah <guimaraess737@gmail.com>
2024-03-12 11:42:53 -0300

    estrutura condicionais

---
+++ 01-intro-python/estrutura-condicionais.py
@@ -0,0 +1,87 @@
+#if/else
+idade = 25
+
+if idade >= 18:
+    print('maior de idade')
+    print('sdad')
+else:
+    print('menor de idade')
+
+# if/elif/else
+# Criança 0 a12, adolecente 13 a 17
+# Adulto 18 a 59, idoso 60+ 
+if idade > 0 and idade <= 12:
+    print('crianca')
+if idade > 12 and idade <= 17:
+    print('adolescente')
+if idade > 17 and idade <= 59:
+    print('Adulto')
+if idade > 59:
+    print('idoso')
+
+if idade <= 12:
+    print('crianca')
+elif idade <= 17:
+    print('adolescente')
+elif idade <= 59:
+    print('adulto')
+else:
+    print('idoso')                
+
+#Condições alinhadas
+email = 'adminemail@gmail.com'
+senha= '123'
+
+if email == 'adminemail@gmail.com':
+    if senha == '123':
+        print('liberado')
+    else:
+        print('erro')
+else:
+    print('erro')
+
+if email == 'adiminemail@gmail.com' and senha == '123':
+        print('liberado')
+else:
+    print('erro')
+
+# entrada eamil, senha
+# saida True or False 
+def libera_acesso(amil, senha):
+    if email == 'adiminemail@gmail.com' and senha == '123':
+        return True
+    else:
+        return False
+
+def liberar_acesso(email, senha):
+     return email == 'adiminemail@gmail.com' and senha == '123'
+
+# entrada hora_atual
+# sim ou não 
+def dentro_horario_comercial (hora_atual):
+    return hora_atual >= 8 and hora_atual <= 18
+
+# dia 1,2,3,4,5,6,7
+# palavra doming, segunda, terca, quarta, quinta, sexta, sábado
+
+dia = 3
+
+if dia ==1:
+    print('domingo')
+elif dia == 2:
+    print('segunda')
+
+lista = ['domingo', 'segunda', 'terca', 'quarta', 'quinta']
+dia = dia - 1
+print(lista[dia])
+
+dias = {
+    1: 'domingo' ,
+    2: 'segunda' ,
+    3: 'terça' ,
+}
+
+if dia in dias:
+    print(dias[dia])
+
+    #ba ba ba, ba ba, baba, ba ba ba, ba ba, baba
