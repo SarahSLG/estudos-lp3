203623eede11021227996d4769ebdea0c66aca2c
Sarah Sabongi <guimaraess737@gmail.com>
2024-06-04 10:46:02 -0300

    modulos e pacotes

---
+++ 03-modulos-pacotes/main.py
@@ -0,0 +1,13 @@
+PI = 3.1415
+
+# se eu coloco print numa função eu a limito
+
+def somar(n1,n2):
+    return 1 + 2
+
+def subtrair(n1,n2):
+    return n1 - n2
+
+print(PI)
+print(somar(10.0,3.2))
+print(subtrair(10.0,3.2))
